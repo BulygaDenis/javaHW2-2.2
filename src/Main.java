1756a03c2629c53aa4e9d09e4470c8fff1ee337c
Neposeda644 <denis_bulyga@mail.ru>
2020-10-13 22:53:12 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        long weight = 76;
+        long height = 176;
+        long BMI service.calculate(weight)
+    }
+}
